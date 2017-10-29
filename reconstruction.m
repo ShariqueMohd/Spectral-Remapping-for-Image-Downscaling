29169d4a05b2e614f0470be75b2162b16cbb50bf
yaghya <you@example.com>
2017-10-29 23:39:58 +0530

    updated reconstruction

--- reconstruction.m
+++ reconstruction.m
@@ -1,12 +1,15 @@
 function [Img] = Reconstruction(Imagename, Q)
 	
-	I=im2double(imread(Imagename));
-	I=rgb2gray(I);
-	S=fftshift(fft2(I));
-
-	R=S-Q;
-	B=(sum(sum(S.*S))-sum(sum(R.*R)))/sum(sum(Q.*Q));
-	b=sqrt(B);
-
+	Siz = size(Im);
+	Filter=getGaussianFilter(Siz(1),Siz(2),2.0);
+    res = zeros(Siz);
+    [x,y] = size(Filter);
+    x = (x+1)/2;
+    y = (y+1)/2;
+    for m=1:Siz(1)
+        for n1:Siz(2)
+            res(i,j,k) = Im(i,j,k) * Filter (i-m+x,j-n+y);
+        end
+    end
 	Img=R + b*Q;
 end
