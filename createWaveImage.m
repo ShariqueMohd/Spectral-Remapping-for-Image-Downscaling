f243d7e13542df01d4c49b6b5330cbd218204602
manan180796 <manan180796>
2017-11-02 20:46:52 +0530

    small changes

--- createWaveImage.m
+++ createWaveImage.m
@@ -4,7 +4,9 @@ tempx = repmat(tempx,[imageSize(2),1]);
 tempy = 1:imageSize(2);
 tempy = tempy';
 tempy = repmat(tempy,[1,imageSize(2)]);
-arg = tempx*wave(2)+tempy*wave(3) +c;
+arg = tempx*wave(2)+tempy*wave(3) +wave(4);
 arg = 2i*pi*arg;
-Result = wave(1)*exp(arg);
+Result = (wave(1)/2)*exp(arg);
+Result = real(Result);
+Result = Result+(wave(1)/2);
 end
--- getAllWaves.m
+++ getAllWaves.m
@@ -1,8 +1,15 @@
-function [ allWaves ] = getAllWaves( image )
+function [ allWaves ] = getAllWaves( image,R )
 %GETALLWAVES Summary of this function goes here
 %   Detailed explanation goes here
-windowFilter = getGaussianFilter(size(image,1),size(image,2),2);
-
+sigma = 10;
+windowFilter = getGaussianFilter(size(image,1),size(image,2),sigma);
+allWaves = cell(size(image,1),size(image,2));
+for i = 1:size(image,1)
+    for j = 1:size(image,2)
+        [i,j]
+        allWaves(i,j) = {GetWaves(image,circshift(windowFilter,[i,j]-1),R)};
+    end
+end
 
 end
 
